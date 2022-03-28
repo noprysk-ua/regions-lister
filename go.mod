0153078a55964e7f30276b3d6a227c9ebff7ceae
CI <ci@example.com--porcelain>
2022-03-28 19:19:52 +0100

    Implementing the regions lister

---
+++ go.mod
@@ -0,0 +1,7 @@
+module regions-lister
+
+go 1.17
+
+require github.com/noprysk-ua/managementapisdk v0.1.1
+
+require github.com/deepmap/oapi-codegen v1.9.1 // indirect
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/noprysk-ua/managementapisdk"
+)
+
+const apiServiceURL = "https://api.singlestore.com"
+
+func main() {
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatal("Environmental variable $API_KEY should be set, visit https://docs.singlestore.com/managed-service/en/reference/management-api.html for details")
+	}
+
+	client, err := managementapisdk.NewClientWithResponses(apiServiceURL,
+		managementapisdk.WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
+			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+			return nil
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	regions, err := client.GetV0betaRegionsWithResponse(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if regions.HTTPResponse.StatusCode != http.StatusOK {
+		log.Fatalf("request returned %s", regions.HTTPResponse.Status)
+	}
+
+	result, err := json.MarshalIndent(regions.JSON200, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", result)
+}

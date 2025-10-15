04f9453bb604abed46d9d98e508820d290be9edf
noprysk-ua <noprysk-ua@signlestore.com>
2025-10-15 16:21:00 +0000

    timout-test

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/singlestore-labs/singlestore-go/management"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const apiServiceURL = "https://api.singlestore.com"
@@ -29,19 +32,132 @@ func main() {
 		log.Fatal(err)
 	}
 
-	regions, err := client.GetV2RegionsWithResponse(context.TODO(), &management.GetV2RegionsParams{})
+	password := "StrongPassword132!"
+	provider := management.AWS
+	allowAllTraffic := true
+	regionName := "eu-west-1"
+	name := "NestorTestACTIVE"
+	r, err := client.PostV1WorkspaceGroupsWithResponse(context.TODO(), management.PostV1WorkspaceGroupsJSONRequestBody{
+		AdminPassword:   &password,
+		AllowAllTraffic: &allowAllTraffic,
+		FirewallRanges:  []string{},
+		Provider:        &provider,
+		RegionName:      &regionName,
+		Name:            name,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if regions.HTTPResponse.StatusCode != http.StatusOK {
-		log.Fatalf("request returned %s", regions.HTTPResponse.Status)
+	if r.StatusCode() != http.StatusOK {
+		log.Fatalf("request returned %s: %s", r.Status(), r.Body)
 	}
 
-	result, err := json.MarshalIndent(regions.JSON200, "", "  ")
+	workspaceGroupID := r.JSON200.WorkspaceGroupID
+
+	defer func() {
+		force := true
+		_, err := client.DeleteV1WorkspaceGroupsWorkspaceGroupIDWithResponse(context.TODO(), workspaceGroupID, &management.DeleteV1WorkspaceGroupsWorkspaceGroupIDParams{Force: &force})
+		if err != nil {
+			log.Printf("failed to delete workspace group %s: %v", workspaceGroupID, err)
+		} else {
+			log.Printf("workspace group %s deleted", workspaceGroupID)
+		}
+	}()
+
+	// WORKSPACE NOW
+
+	// workspaceGroupID := uuid.MustParse("b3a3a333-0f83-4c8e-b86c-f793d429f47a")
+	resp, err := client.PostV1WorkspacesWithResponse(context.TODO(), management.PostV1WorkspacesJSONRequestBody{
+		Name:             "nestor-test",
+		WorkspaceGroupID: workspaceGroupID,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%s\n", result)
+	if resp.StatusCode() != http.StatusOK {
+		log.Fatalf("request returned %s: %s", resp.Status(), resp.Body)
+	}
+
+	workspaceID := resp.JSON200.WorkspaceID
+
+	defer func() {
+		_, err := client.DeleteV1WorkspacesWorkspaceIDWithResponse(context.TODO(), workspaceID)
+		if err != nil {
+			log.Printf("failed to delete workspace %s: %v", workspaceID, err)
+		} else {
+			log.Printf("workspace %s deleted", workspaceID)
+		}
+	}()
+
+	// TODO: To test manually, uncomment
+	// workspaceID := uuid.MustParse("c1e1dfb9-f458-4ac1-a8c1-9642230edde5")
+
+	var response *management.GetV1WorkspacesWorkspaceIDResponse
+	for {
+		var err error
+		response, err = client.GetV1WorkspacesWorkspaceIDWithResponse(context.TODO(), workspaceID, &management.GetV1WorkspacesWorkspaceIDParams{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if response.StatusCode() != http.StatusOK {
+			log.Fatalf("request returned %s: %s", response.Status(), response.Body)
+		}
+
+		if response.JSON200.State == management.WorkspaceStateACTIVE {
+			break
+		} else {
+			fmt.Printf("Workspace status is %s, waiting to become ACTIVE\n", response.JSON200.State)
+		}
+
+		time.Sleep(3 * time.Second)
+	}
+
+	fmt.Printf("Workspace %s is ACTIVE\n", response.JSON200.WorkspaceID)
+
+	activeAt := time.Now()
+
+	endpoint := *response.JSON200.Endpoint
+	password = "StrongPassword132!"
+	user := "admin"
+
+	connString := fmt.Sprintf(
+		"%s:%s@tcp(%s)/%s?timeout=3s", // With timeout
+		user,
+		password,
+		endpoint,
+		"information_schema",
+	)
+
+	dbConn, err := sqlx.ConnectContext(context.TODO(), "mysql", connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer dbConn.Close()
+
+	rows, err := dbConn.QueryContext(context.TODO(), "select 1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var tmp int
+
+		err := rows.Scan(&tmp)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("select 1 returned %d\n", tmp)
+	}
+
+	executedAt := time.Now()
+
+	fmt.Printf("It took %d seconds between ACTIVE and EXECUTED\n", int(executedAt.Sub(activeAt).Seconds()))
+
+	fmt.Printf("Workspace %s is ACTIVE\n", response.JSON200.WorkspaceID)
 }
